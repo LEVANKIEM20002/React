24c89409130425e80dc38d76c93c7d672783c0cf
Le Kiem <lvkkiem2002@gmail.com>
2022-10-04 02:43:16 +0700

    Update doctor

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -12,14 +12,18 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 class HomePage extends Component {
+    // handleAfterChange = (event, slick, currentSlide) =>{
+
+    // }
 
     render() {
         let settings = {
             dots: false,
-            infinite: true,
+            infinite: false,
             speed: 500,
             slidesToShow: 4,
             slidesToScroll: 1,
+            // afterChange: this.handleAfterChange
           };
 
 
