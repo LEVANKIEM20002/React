f56bec86997e787534632ce30ba17eaddd4568fa
Le Kiem <lvkkiem2002@gmail.com>
2022-10-13 00:07:39 +0700

    #78 Add Information Save Interface

--- src/store/reducers/adminReducer.js
+++ src/store/reducers/adminReducer.js
@@ -8,7 +8,9 @@ const initialState = {
   users: [],
   topDoctors: [],
   allDoctors: [],
-  allScheduleTime:[],
+  allScheduleTime: [],
+
+  allRequiredDoctorInfor: [],
 };
 
 const adminReducer = (state = initialState, action) => {
@@ -24,14 +26,14 @@ const adminReducer = (state = initialState, action) => {
       state.isLoadingGender = false;
       return {
         ...state,
-      };    
+      };
     case actionTypes.FETCH_GENDER_FAILED:
       state.isLoadingGender = false;
       state.genders = [];
       return {
         ...state,
       };
-  
+
     case actionTypes.FETCH_POSITION_SUCCESS:
       state.positions = action.data;
       return {
@@ -86,18 +88,29 @@ const adminReducer = (state = initialState, action) => {
       return {
         ...state,
       };
-      
-      case actionTypes.FETCH_ALLCODE_SCHEDULE_TIME_SUCCESS:
-        state.allScheduleTime = action.dataTime;
-        return {
-          ...state,
-        };
-      case actionTypes.FETCH_ALLCODE_SCHEDULE_TIME_FAILED:
-        state.allScheduleTime = [];
-        return {
-          ...state,
-        };
 
+    case actionTypes.FETCH_ALLCODE_SCHEDULE_TIME_SUCCESS:
+      state.allScheduleTime = action.dataTime;
+      return {
+        ...state,
+      };
+    case actionTypes.FETCH_ALLCODE_SCHEDULE_TIME_FAILED:
+      state.allScheduleTime = [];
+      return {
+        ...state,
+      };
+
+    case actionTypes.FETCH_REQUIRED_DOCTOR_INFOR_SUCCESS:
+      state.allRequiredDoctorInfor = action.data;
+
+      return {
+        ...state,
+      };
+    case actionTypes.FETCH_REQUIRED_DOCTOR_INFOR_FAILED:
+      state.allRequiredDoctorInfor = [];
+      return {
+        ...state,
+      };
     default:
       return state;
   }
