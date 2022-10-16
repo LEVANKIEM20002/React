967cdc4eeb0df6992fa48d239aee8364df9ddf96
Le Kiem <lvkkiem2002@gmail.com>
2022-10-16 17:25:47 +0700

    #83 Write API Dynamic Loading Doctor Information

--- src/containers/Patient/Doctor/Modal/BookingModal.js
+++ src/containers/Patient/Doctor/Modal/BookingModal.js
@@ -3,6 +3,8 @@ import { connect } from "react-redux";
 import { FormattedMessage } from "react-intl";
 import "./BookingModal.scss";
 import { Modal } from "reactstrap";
+import ProfileDoctor from "../ProfileDoctor";
+import _ from "lodash";
 
 class BookingModal extends Component {
   constructor(props) {
@@ -19,6 +21,10 @@ class BookingModal extends Component {
 
   render() {
     let { isOpenModal, closeBookingModal, dataTime } = this.props;
+    let doctorId = "";
+    if (dataTime && !_.isEmpty(dataTime)) {
+      doctorId = dataTime.doctorId;
+    }
     return (
       <div>
         {/* toggle={} */}
@@ -38,8 +44,10 @@ class BookingModal extends Component {
             </div>
             <div className="booking-modal-body">
               {/* {JSON.stringify(dataTime)} */}
-              <div className="doctor-infor"></div>
-              <div className="price">Giá khám 500000</div>
+              <div className="doctor-infor">
+                <ProfileDoctor doctorId={doctorId} />
+              </div>
+
               <div className="row">
                 <div className="col-6 form-group">
                   <label>Họ tên</label>
